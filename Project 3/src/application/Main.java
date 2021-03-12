f726d3ee533bdb9e23bd1b7e9931d67a021d16a5
Gent B <gentblaku@gmail.com>
2021-03-11 23:09:55 -0500

    More progress

--- Project 3/src/application/Main.java
+++ Project 3/src/application/Main.java
@@ -1,18 +1,27 @@
 package application;
 	
+import java.io.IOException; 
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+//import view.Controller;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+
 
 
 public class Main extends Application {
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("View.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Gent's Big Boss Company of Sexy People!");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
